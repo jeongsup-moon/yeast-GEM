56b1295658de58575d50729f47cd98d886d59897
eiden309 <59604678+eiden309@users.noreply.github.com>
2020-06-07 17:50:31 +0800

    feat: function and data to fix rxnDirection

---
+++ ComplementaryScripts/modelCuration/changerxnDirection.m
@@ -0,0 +1,57 @@
+% This script modifies reaction direction based on data from 
+% rxnDirectionInfo.tsv
+%
+% rxnDirectionInfo.tsv is a compilation of deltaG, reversibility index and
+% reaction direction information from various data sources, generated via
+% checkrxnDirection.m. The tsv file is edited to add 3 new columns
+% 'Action', 'New_model.lb' and 'Notes'
+%
+% Inputs: model and rxnDirectionInfo.tsv
+%
+% Cheng Wei Quan (Eiden), 2020-05-05
+
+%Load model
+cd ..
+model = loadYeastModel;
+
+%Load rxnDirectionInfo.tsv files
+fid = fopen('../ComplementaryData/modelcuration/rxnDirectionInfo.tsv');
+format = repmat('%s ',1,16);
+format = strtrim(format);
+temp = textscan(fid,format,'Delimiter','\t','HeaderLines',0);
+for i = 1:length(temp)
+    rxnDirectionInfo(:,i) = temp{i}; %use {} instead of () for cell array
+end
+commentLines = startsWith(rxnDirectionInfo(:,1),'#');
+rxnDirectionInfo(commentLines,:) = [];
+fclose(fid);
+
+%Store required variables
+rxns = rxnDirectionInfo(:,1);
+[~,rxn_idx] = ismember(rxns,model.rxns);
+action = rxnDirectionInfo(:,14);
+new_lb = rxnDirectionInfo(:,15);
+
+%Change rxnDirection/coefficient sign based on action
+for i = 1:size(action,1)
+    if strcmp(action(i),'Change to irreversible forward')
+        model.lb(rxn_idx(i)) = str2num(new_lb{i});
+        model.rxnNotes(rxn_idx(i)) = join([model.rxnNotes(rxn_idx(i)),'| rxnDirection curated (PR #227)']);
+    elseif strcmp(action(i),'Change to irreversible forward and perform sign change for all metabolites in this reaction')
+        model.lb(rxn_idx(i)) = str2num(new_lb{i});
+        met_idx = find(model.S(:,rxn_idx(i)));
+        model.S(met_idx,rxn_idx(i)) = -model.S(met_idx,rxn_idx(i));
+        model.rxnNotes(rxn_idx(i)) = join([model.rxnNotes(rxn_idx(i)),'| rxnDirection curated (PR #227)']);
+    end
+end
+
+%Remove whitespace(s) when adding notes
+model.metNotes(:) = strtrim(model.metNotes(:));
+model.rxnNotes(:) = strtrim(model.rxnNotes(:));
+
+%Remove leading '| ' in notes that were previously empty
+model.rxnNotes = regexprep(model.rxnNotes,'^\| ','');
+model.metNotes = regexprep(model.metNotes,'^\| ','');
+
+%Save model
+saveYeastModel(model);
