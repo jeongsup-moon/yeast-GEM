4f36a9909db1c214d1d7880c26bba746f80ab0e4
Benjamin Sanchez <bjsanchezb@gmail.com>
2020-09-04 16:27:02 +0200

    fix-data: delete compartment info from met file
    not needed, as annotation will be the same regardless of compartment and can be mapped using metabolites in the matrix file

--- ComplementaryScripts/modelCuration/addFAEnewRxn.m
+++ ComplementaryScripts/modelCuration/addFAEnewRxn.m
@@ -75,19 +75,22 @@ newmet_annot = textscan(fid,'%s %s %s %s %s %s %s','Delimiter','\t','HeaderLines
 newmet.metNames    = newmet_annot{1};
 newmet.metFormulas = newmet_annot{2};
 newmet.metCharges  = cellfun(@str2num, newmet_annot{3});
-newmet.metKEGGID   = newmet_annot{5};
-newmet.metChEBIID  = newmet_annot{6};
-newmet.metNotes    = newmet_annot{7};
+newmet.metKEGGID   = newmet_annot{4};
+newmet.metChEBIID  = newmet_annot{5};
+newmet.metNotes    = newmet_annot{6};
 fclose(fid);
 
 for i = 1:length(newmet.metNames)
-    [~,metID] = ismember(newmet.metNames(i),model.metNames);
-    if metID ~= 0
-        model.metFormulas{metID} = newmet.metFormulas{i};
-        model.metCharges(metID)  =  newmet.metCharges(i);
-        model.metKEGGID{metID}   = newmet.metKEGGID{i};
-        model.metChEBIID{metID}  = newmet.metChEBIID{i};
-        model.metNotes{metID}    = ['NOTES: added after new annotation (PR #142); ',newmet.metNotes{i}];
+    mets = strcmp(newmet.metNames(i),matrix.metIDs);
+    for j = 1:length(mets)
+        if mets(j)
+            [~,metID] = ismember(matrix.metnames(j),model.metNames);
+            model.metFormulas{metID} = newmet.metFormulas{i};
+            model.metCharges(metID)  =  newmet.metCharges(i);
+            model.metKEGGID{metID}   = newmet.metKEGGID{i};
+            model.metChEBIID{metID}  = newmet.metChEBIID{i};
+            model.metNotes{metID}    = ['NOTES: added after new annotation (PR #142); ',newmet.metNotes{i}];
+        end
     end
 end
 
