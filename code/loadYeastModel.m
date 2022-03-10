90bca651d791405274f21c9c40ea913cd3b3f58c
Eduard Kerkhoven <eduardk@chalmers.se>
2022-03-10 23:49:08 +0100

    feat: loadYeastModel supports conversion to COBRA format

--- code/loadYeastModel.m
+++ code/loadYeastModel.m
@@ -1,14 +1,23 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% model = loadYeastModel
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 function model = loadYeastModel
+% loadYeastModel
+%   Load the yeast-GEM in a MATLAB environment, requires the RAVEN Toolbox
+%   and libSBML to be installed. By default, the model is provided in RAVEN
+%   format by default, but can alternatively be converted to COBRA format.
+%
+%   Input:
+%       cobra       true if the model should be provided as COBRA format,
+%                   false for RAVEN format (optional, default false)
+%
+%   Output:
+%       model       the yeast-GEM model structure
+%
+%   Usage: model = loadYeastModel(cobra)
 
 scriptFolder = fileparts(which(mfilename));
 currentDir = cd(scriptFolder);
 model = importModel('../model/yeast-GEM.xml');
 cd(currentDir)
-
+if nargin>0 && cobra==true
+    model = ravenCobraWrapper(model);
 end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
