204b568e41017996cd49971641c62e846297d221
Eduard Kerkhoven <eduardk@chalmers.se>
2022-05-26 22:20:48 +0200

    feat: consolidated curation between releases

---
+++ code/modelCuration/curationsOnV8_6_0.m
@@ -0,0 +1,73 @@
+% This scripts applies curations to be applied on yeast-GEM release 8.6.0.
+% Indicate which Issue/PR are addressed
+
+%% Load yeast-GEM 8.6.0 (requires local yeast-GEM git repository)
+cd ..
+model = getEarlierModelVersion('8.6.0');
+model.id='yeast-GEM_develop';
+
+%% Curate complex annotation (PR #305)
+% Add new genes
+newModel       = curateMetsRxnsGenes(model,'none','../data/modelCuration/curationsOnV8_6_0/complexAnnotationGenes.tsv');
+
+% Add gene standard name for new genes
+fid = fopen('../data/modelCuration/curationsOnV8_6_0/complexAnnotation.tsv');
+complexAnnot = textscan(fid,'%q %q %q %q %q %q %q','Delimiter','\t','HeaderLines',1);
+fclose(fid);
+newGPR.ID     = complexAnnot{1};
+newGPR.GPR    = complexAnnot{3};
+newModel=changeGrRules(newModel,newGPR.ID,newGPR.GPR);
+
+% Delete unused genes (if any)
+newModel = deleteUnusedGenes(newModel);
+
+%% Curate gene association for transport rxns (PR #306)
+% Add new genes
+newModel       = curateMetsRxnsGenes(newModel,'none','../data/modelCuration/curationsOnV8_6_0/transRxnNewGPRGenes.tsv');
+
+% Change GPR relations
+fid           = fopen('../data/modelCuration/curationsOnV8_6_0/TransRxnNewGPR.tsv');
+changegpr     = textscan(fid,'%q %q %q %q %q %q %q %q','Delimiter','\t','HeaderLines',1);
+newGPR.ID     = changegpr{1};
+newGPR.GPR    = changegpr{2};
+fclose(fid);
+
+newModel=changeGrRules(newModel,newGPR.ID,newGPR.GPR);
+
+% Delete unused genes (if any)
+newModel = deleteUnusedGenes(newModel);
+
+%% Add new gene associations from databases (PR #313)
+% Add new genes
+newModel       = curateMetsRxnsGenes(model,'none','../data/modelCuration/curationsOnV8_6_0/newGPRsfromDBsGenes.tsv');
+
+% Change GPR relations
+fid           = fopen('../data/modelCuration/curationsOnV8_6_0/newGPRsfromDBs.tsv');
+changegpr     = textscan(fid,'%q %q %q %q %q %q %q %q','Delimiter','\t','HeaderLines',1);
+newGPR.ID     = changegpr{1};
+newGPR.GPR    = changegpr{3};
+fclose(fid);
+
+newModel=changeGrRules(newModel,newGPR.ID,newGPR.GPR);
+
+% Delete unused genes (if any)
+newModel = deleteUnusedGenes(newModel);
+
+% Show some metrics:
+cd modelTests
+disp('Run gene essentiality analysis')
+[new.accuracy,new.tp,new.tn,new.fn,new.fp] = essentialGenes(newModel);
+fprintf('Genes in model: %d\n',numel(newModel.genes));
+fprintf('Gene essentiality accuracy: %d\n', new.accuracy);
+fprintf('Gene essentiality TP: %d\n', numel(new.tp));
+fprintf('Gene essentiality TN: %d\n', numel(new.tn));
+fprintf('Gene essentiality FP: %d\n', numel(new.fp));
+fprintf('Gene essentiality FN: %d\n', numel(new.fn));
+disp('Run growth analysis')
+R2=growth(newModel);
+fprintf('R2 of growth prediction: %d\n', R2);
+
+% Save model:
+cd ..
+saveYeastModel(newModel)
+cd modelCuration
